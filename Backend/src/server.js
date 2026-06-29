61209ba381c66e2ff2e4ae62c5c0cdeb4c0b483d
Tom-1747 <hei.damien.3@gmail.com>
2026-06-29 17:56:45 +0300

    Delete(backend): delete the backend for the test

--- Backend/src/server.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import app from './index.js';
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
-});
